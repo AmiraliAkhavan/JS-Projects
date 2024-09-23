ab6cd2416a5a83f2096f64caec3902da1c465ad6
AmiraliAkhavan <111018923+AmiraliAkhavan@users.noreply.github.com>
2024-09-23 21:46:01 +0330

    done

---
+++ script.js
@@ -0,0 +1,8 @@
+const faq = document.querySelectorAll(".faq");
+const toggle = document.querySelectorAll(".faq-toggle");
+
+toggle.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.parentNode.classList.toggle("active");
+  });
+});
